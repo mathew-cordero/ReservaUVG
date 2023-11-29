c56e714a849f84fc3433affe6ace9f200dcf3a16
mathew-cordero <mcorderoeb5bm2019071@gmail.com>
2023-11-29 03:38:08 -0600

    Agregar inicio de sesion

---
+++ app/src/main/java/com/example/reservauvg/Auth/Login.kt
@@ -0,0 +1,12 @@
+package com.example.reservauvg.Auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.reservauvg.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/example/reservauvg/MainActivity.kt
+++ app/src/main/java/com/example/reservauvg/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.reservauvg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.reservauvg.Auth.Login
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent = Intent(this, Login::class.java)
+        startActivity(intent)
     }
 }
