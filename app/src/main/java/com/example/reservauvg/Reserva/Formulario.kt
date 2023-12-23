4c4a4981826d4b72d4c11064c5b1ba34fa8a371d
mathew-cordero <mcorderoeb5bm2019071@gmail.com>
2023-12-23 03:33:42 -0600

    Agregar datepicker

--- app/src/main/java/com/example/reservauvg/Reserva/Formulario.kt
+++ app/src/main/java/com/example/reservauvg/Reserva/Formulario.kt
@@ -2,10 +2,19 @@ package com.example.reservauvg.Reserva
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.reservauvg.R
+import com.example.reservauvg.components.DatePickerFragment
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class Formulario : AppCompatActivity() {
+    private lateinit var fechatextView: TextInputEditText
+    private var fechaagendada:String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (getSupportActionBar() != null) {
@@ -17,6 +26,17 @@ class Formulario : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         supportActionBar?.setIcon(R.drawable.rulogo_decolor)
+
+        //vamos a llamar a los widgets
+        val iconbtncalendar: ImageView = findViewById(R.id.form_fecha_icon)
+        val iconbtnhrinicio: ImageView = findViewById(R.id.form_hora_inicio_icon)
+        val iconbtnhrfinal:ImageView = findViewById(R.id.form_hora_final_icon)
+        fechatextView = findViewById(R.id.form_fecha)
+
+        //vamos a hacer los setonclick de las imagenes
+        iconbtncalendar.setOnClickListener {
+            showDatePickerDialog()
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -29,5 +49,28 @@ class Formulario : AppCompatActivity() {
             // Agregar más casos según sea necesario
             else -> return super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun showDatePickerDialog() {
+        val datePicker = DatePickerFragment { day, month, year -> onDateSelected(day, month, year) }
+        datePicker.show(supportFragmentManager, "datePicker")
+    }
+
+    private fun onDateSelected(year: Int, month: Int, day: Int) {
+
+        var daymodify: String = day.toString()
+        var monthmodify:String = (month+1).toString()
+
+        if(day<=9){
+            daymodify = "0$daymodify"
+        }
+        if((month+1)<=9){
+            monthmodify = "0$monthmodify"
+        }
+        val formattedDate = "$year-$monthmodify-$daymodify"
+        // Convierte la cadena en un objeto Editable
+        val editableText = Editable.Factory.getInstance().newEditable(formattedDate)
+        // Establece el texto en el TextInputEditText
+        fechatextView.text = editableText
     }
 }
---
+++ app/src/main/java/com/example/reservauvg/components/DatePickerFragment.kt
@@ -0,0 +1,26 @@
+package com.example.reservauvg.components
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+import java.util.Calendar
+
+class DatePickerFragment(val listener: (day: Int, month: Int, year: Int) -> Unit):DialogFragment(),
+    DatePickerDialog.OnDateSetListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c = Calendar.getInstance()
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        val month = c.get(Calendar.MONTH)
+        val year = c.get(Calendar.YEAR)
+        val picker = DatePickerDialog(activity as Context,this,year,month,day)
+        return picker
+    }
+
+    override fun onDateSet(view: DatePicker?, dayofMonth: Int, month: Int, year: Int) {
+        listener(dayofMonth,month,year)
+    }
+}
